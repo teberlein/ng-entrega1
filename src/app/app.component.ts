e26ba96249f15e075cd8a7002613bf51f9b28587
tatoeberlein <tomitatoeber@gmail.com>
2024-02-01 19:41:18 -0300

    saludador component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SaludadorComponent } from './saludador/saludador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SaludadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
